21571a73362e0948ea0aa3226fc311ff649affd5
roma-i <romanivanyo@gmail.com>
2018-10-22 09:19:40 +0300

    Update cookies.js

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -2,16 +2,11 @@
 	cookie_section = $('.cookies-section');
 
 	//Accept Cookies
-	$('.accept-button').on('click', function(e) {
+	$('.accept-button, .close-button').on('click', function(e) {
 		e.preventDefault();
 
 		$.cookie("accept-cookies", true, { expire: 365 });
 		cookie_section.hide(800);
 	});
-
-	//Close Cookies
-	$('.close-button').on('click', function(e) {
-		cookie_section.hide(800);
-	})
 
 }) (jQuery)
